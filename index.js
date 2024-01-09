ba33204f58a0abb1ae49a9d4017e853653fcc1c1
bradleymbuvi <bradwent@gmail.com>
2024-01-09 12:13:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(record) {
+    
+    if (record.result === "W") {
+     return (record.year)
+    } 
+ }
+
+ const finalResult = record.find(superbowlWin).year
+ 
+
+ function superbowlWin(record) {
+    record.result === 'W'
+    }
+
+
+
+    record.find(superbowlWin).year;
